07ea820c8dca29088070a67a92a090f09df5767e
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2023-01-01 19:43:29 +0700

    Update menu-awal.js

--- plugins/menu-awal.js
+++ plugins/menu-awal.js
@@ -9,7 +9,7 @@ contextInfo: { externalAdReply :{
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'ʟᴀɴꜱ ᴛᴀᴍᴠᴀɴ',          previewType: 0,
+                        body: 'Hai Ngabs',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
                         sourceUrl: sig
                       }}
